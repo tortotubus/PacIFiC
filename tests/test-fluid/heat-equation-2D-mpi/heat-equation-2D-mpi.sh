4845c2573cb706363f78c94c523a730fb1fc9fc4
Damien <huet@math.ubc.ca>
2021-06-01 14:57:07 -0700

    attempt 2 of proper success test of the heat equation

--- tests/test-fluid/heat-equation-2D-mpi/heat-equation-2D-mpi.sh
+++ tests/test-fluid/heat-equation-2D-mpi/heat-equation-2D-mpi.sh
@@ -6,7 +6,7 @@
 # Assess success: compare to reference file (this command's exit status returns 0 if the two files are identical)
 for time in {1..2}
 do
-  for proc in {1..4}
+  for proc in {0..3}
   do
     cmp Res/saveT${time}_${proc}.vtr Res_ref/ref_saveT${time}_${proc}.vtr ; echo $? >> heat-equation-2D-mpi-success.txt
   done
