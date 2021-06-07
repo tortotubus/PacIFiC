14e00f82a5cb7008d86e4314ad59630abeb11b9c
Damien <huet@math.ubc.ca>
2021-06-07 11:15:37 -0700

    Added verbose in success assessment of heat-equation-2D-mpi and a partial README file. Probably the last commit of this branch

---
+++ tests/test-fluid/heat-equation-2D-mpi/README.md
@@ -0,0 +1,10 @@
+# Heat equation 2D MPI
+
+This test case checks the computation of the 2D Laplacian of the Cartesian solver.
+
+## Set up
+[To be completed]
+
+## Success assessment
+All the files located in Reference_results are compared.
+
--- tests/test-fluid/heat-equation-2D-mpi/heat-equation-2D-mpi.sh
+++ tests/test-fluid/heat-equation-2D-mpi/heat-equation-2D-mpi.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# Run test case on four cores
+# 1) Run test case on four cores
 ./pacific ./InputFiles/time.mac 4
 
-# Assess success: compare to reference file (this command's exit status returns 0 if the two files are identical)
+# 2) Assess sucess
+echo "Success assessment output:"
+
+# 2.i) Compare to reference file (this command's exit status returns 0 if the two files are identical)
 for time in {1..2}
 do
   for proc in {0..3}
   do
-    cmp Res/saveT${time}_${proc}.vtr Res_ref/ref_saveT${time}_${proc}.vtr ; echo $? >> heat-equation-2D-mpi-success.txt
+    cmp Res/saveT${time}_${proc}.vtr Reference_results/ref_saveT${time}_${proc}.vtr ; echo $? >> heat-equation-2D-mpi-success.txt
+    output="ref_saveT${time}_${proc}.vtr: "
+    output+=$(tail -n 1 heat-equation-2D-mpi-success.txt)
+    echo $output
   done
 done
 
-echo "Success assessment output:"
-cat heat-equation-2D-mpi-success.txt
+# 2.ii) We run a command that fails if any of the above success assessments failed 
 $(! grep -q "1" heat-equation-2D-mpi-success.txt)
