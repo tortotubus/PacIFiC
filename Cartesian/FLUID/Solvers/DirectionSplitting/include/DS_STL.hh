59f16fe1df068bf0642bfb592772e70e4bc83406
Aashish Goyal <aashish.goyal@alumni.ubc.ca>
2022-07-04 12:05:26 -0700

    Empty classes of STL for Antoine to fill.

---
+++ Cartesian/FLUID/Solvers/DirectionSplitting/include/DS_STL.hh
@@ -0,0 +1,240 @@
+#ifndef _DS_RIGIDBODY__
+#define _DS_RIGIDBODY__
+
+#include <geomVector.hh>
+#include <size_t_vector.hh>
+#include <size_t_array2D.hh>
+#include <doubleArray2D.hh>
+#include <boolVector.hh>
+#include <MAC_assertions.hh>
+#include <MAC_Communicator.hh>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <iostream>
+#include <fstream>
+using std::ostream;
+using std::vector;
+using std::tuple;
+class FS_RigidBody;
+class FV_DiscreteField;
+class FV_Mesh;
+
+/** @brief The class DS_RigidBody.
+
+A moving or stationary rigid body in the Direction Splitting solver.
+
+@author A. Wachs - Pacific project 2021 */
+
+class DS_RigidBody
+{
+   public: //-----------------------------------------------------------------
+
+   //-- Constructors & Destructor
+
+      /**@name Constructors & Destructor */
+      //@{
+      /** @brief Default constructor */
+      DS_RigidBody();
+
+      /** @brief Constructor with arguments
+      @param pgrb pointer to the corresponding geometric rigid body */
+      DS_RigidBody( FS_RigidBody* pgrb );
+
+      /** @brief Destructor */
+      virtual ~DS_RigidBody();
+      //@}
+
+
+   //-- Set methods
+
+      /**@name Set methods */
+      //@{
+      /** @brief Updates the rigid body features from its corresponding
+      geometric rigid body */
+      virtual void update() = 0;
+      //@}
+
+   //-- Get Methods
+      /**@name Get methods */
+      //@{
+      /** @brief Return the halo zones of the rigid body */
+      vector<geomVector*> get_rigid_body_haloZone( ) const;
+
+      /** @brief Returns rigid body velocity including rotation speed at pt
+      @param pt the point */
+      geomVector get_rigid_body_velocity( geomVector const& pt ) const;
+
+      /** @brief Returns rigid body angular velocity */
+      geomVector get_rigid_body_angular_velocity( ) const;
+
+      /** @brief Returns pointer to the rigid body gravity center */
+      geomVector const* get_ptr_to_gravity_centre( ) const;
+
+      /** @brief Returns circumscribed radius */
+      double get_circumscribed_radius( ) const;
+
+      /** @brief Returns a tuple of mass and density of RB */
+      std::tuple<double,double> get_mass_and_density() const;
+
+      /** @brief Returns the distance of a point with the rigid body
+      with a given ray vector and source
+      @param pt the point
+      @param direction x, y or z (=0, 1 or 2)
+      @param positive true if search in the positive direction of the coordinate
+      axis and false otherwise */
+      double get_distanceTo( geomVector const& source,
+                             geomVector const& rayDir,
+                             double const& delta ) const;
+
+      /** @brief Return the surface points on the rigid body */
+      vector<geomVector*> get_rigid_body_surface_points( ) const;
+
+      /** @brief Return the area of the surface points on the rigid body */
+      vector<geomVector*> get_rigid_body_surface_areas( ) const;
+
+      /** @brief Return the normal of the surface points on the rigid body */
+      vector<geomVector*> get_rigid_body_surface_normals( ) const;
+
+      //@}
+
+   //-- Methods
+
+      /**@name Methods */
+      //@{
+      /** @brief Writes the attributes in a stream
+      @param out output stream
+      @param indent_width indentation width */
+      virtual void display( ostream& out, size_t const& indent_width )
+      	const = 0;
+
+      /** @brief Compute the halozone of a rigid body
+      @param out output stream
+      @param indent_width indentation width */
+      virtual void compute_rigid_body_halozone( ) = 0;
+
+      /** @brief Returns whether a point is inside the rigid body
+      @param pt the point */
+      bool isIn( geomVector const& pt ) const;
+
+      /** @brief Returns whether a point is inside the rigid body
+      @param x x-coordinate of the point
+      @param y x-coordinate of the point
+      @param z x-coordinate of the point */
+      bool isIn( double const& x, double const& y, double const& z ) const;
+
+      /** @brief Returns the level set value of a point from the rigid body
+      @param pt the point */
+      double level_set_value( geomVector const& pt ) const;
+
+      /** @brief Returns the level set value of a point from the rigid body
+      @param x x-coordinate of the point
+      @param y x-coordinate of the point
+      @param z x-coordinate of the point */
+      double level_set_value( double const& x
+                            , double const& y
+                            , double const& z ) const;
+
+      /** @brief Compute the surface points by discretizing the rigid body
+      surface in approximately equal areas (if possible) */
+      virtual void compute_surface_points( ) = 0;
+
+
+      /** @brief Compute number of points on a rigid body
+      @param surface_cell_scale scale of surface cell compared with the grid
+      @param dx grid size */
+      virtual void compute_number_of_surface_variables(
+               double const& surface_cell_scale, double const& dx ) = 0;
+
+      /** @brief Write the surface discretization points of all RB in
+      respective CSV files */
+      void write_surface_discretization( const std::string& file );
+
+      /** @brief Correct surface discretization due to PBC */
+      void correct_surface_discretization( FV_Mesh const* MESH );
+
+      /** @brief Translate the surface points already discretized at
+      the start of simulation */
+      void translate_surface_points( geomVector const& delta);
+
+      /** @brief Initializa the surface variables for a rigid body
+      such as points, normals, and area */
+      void initialize_surface_variables( );
+
+      /** @brief Update the pressure force on a surface point
+      @param i index of the surface point
+      @param value the value to assign for pressure force */
+      void update_Pforce_on_surface_point( size_t const& i
+                                         , geomVector const& value );
+
+      /** @brief Update the viscous force on a surface point
+      @param i index of the surface point
+      @param value the value to assign for viscous force */
+      void update_Vforce_on_surface_point( size_t const& i
+                                         , geomVector const& value );
+
+      /** @brief Update the temperature gradient on a surface point
+      @param i index of the surface point
+      @param value the value to assign for temperature gradient */
+      void update_Tgrad_on_surface_point( size_t const& i
+                                        , double const& value );
+
+      /** @brief Update the RB position and velocity
+      @param pos updated position
+      @param vel updated translation velocity */
+      void update_RB_position_and_velocity(geomVector const& pos,
+                                           geomVector const& vel,
+                                           geomVector const& ang_vel,
+                         vector<geomVector> const& periodic_directions);
+
+      //@}
+
+
+   protected: //--------------------------------------------------------------
+
+   //-- Attributes
+
+      /**@name Parameters */
+      //@{
+      FS_RigidBody* m_geometric_rigid_body; /**< Pointer to the corresponding
+    	geometric rigid body */
+      vector<geomVector*> m_surface_points; /**< vector of points distributed on
+      	the surface of the particle to compute surface integrals */
+      vector<geomVector*> m_surface_area; /**< vector of the area associated
+         with the points distributed on the surface of the particle */
+      vector<geomVector*> m_surface_normal; /**< vector of the normal associated
+         with the points distributed on the surface of the particle */
+      vector<geomVector> m_surface_Pforce; /**< vector of the pressure force
+         on the points distributed on the surface of the particle */
+      vector<geomVector> m_surface_Vforce; /**< vector of the viscous force
+         on the points distributed on the surface of the particle */
+      vector<double> m_surface_Tgrad; /**< vector of the temperature gradient
+         on the points distributed on the surface of the particle */
+      vector<geomVector*> m_halo_zone; /**< vector of min and max extents
+         of rigid body halozone, required for void fraction detection */
+      size_t Ntot; /** < Stores the total number of surface points on a rigid
+         body */
+      //@}
+
+
+   //-- Methods
+
+      /**@name Methods */
+      //@{
+
+      //@}
+
+
+   private: //----------------------------------------------------------------
+
+   //-- Constructors & Destructor
+
+      /**@name Constructors & Destructor */
+      //@{
+      /** @brief Copy constructor
+      @param copy copied DS_RigidBody object */
+      DS_RigidBody( DS_RigidBody const& copy );
+      //@}
+};
+
+#endif
---
+++ Cartesian/FLUID/Solvers/DirectionSplitting/src/DS_STL.cc
@@ -0,0 +1,398 @@
+#include <DS_STL.hh>
+#include <FV_DiscreteField.hh>
+#include <FV_Mesh.hh>
+#include <MAC.hh>
+using std::endl;
+
+
+//---------------------------------------------------------------------------
+DS_STL:: DS_STL()
+//---------------------------------------------------------------------------
+  : m_geometric_rigid_body( NULL )
+{
+  MAC_LABEL( "DS_STL:: DS_STL" ) ;
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+DS_STL:: DS_STL( FS_RigidBody* pgrb )
+//---------------------------------------------------------------------------
+  : m_geometric_rigid_body( pgrb )
+{
+  MAC_LABEL( "DS_STL:: DS_STL" ) ;
+
+
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+DS_STL:: ~DS_STL()
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: ~DS_STL" ) ;
+
+  if ( !m_surface_points.empty() ) m_surface_points.clear();
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+bool DS_STL:: isIn( geomVector const& pt ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: isIn(pt)" ) ;
+
+  // return ( m_geometric_rigid_body->isIn( pt ) );
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+bool DS_STL:: isIn( double const& x, double const& y, double const& z )
+	const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: isIn(x,y,z)" ) ;
+
+  // return ( m_geometric_rigid_body->isIn( x, y, z ) );
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+double DS_STL:: level_set_value( geomVector const& pt ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: level_set_value(pt)" ) ;
+
+  // return ( m_geometric_rigid_body->level_set_value( pt ) );
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+double DS_STL:: level_set_value( double const& x
+                                     , double const& y
+                                     , double const& z ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: level_set_value(x,y,z)" ) ;
+
+  // return ( m_geometric_rigid_body->level_set_value( x, y, z ) );
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+double DS_STL:: get_distanceTo( geomVector const& source,
+                                      geomVector const& rayDir,
+                                      double const& delta ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_distanceTo" ) ;
+
+  // return (m_geometric_rigid_body->distanceTo(source, rayDir, delta));
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+geomVector DS_STL:: get_rigid_body_velocity( geomVector const& pt ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: rigid_body_velocity(pt)" ) ;
+
+  geomVector value(0.,0.,0.);
+
+  return (value);
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+geomVector DS_STL:: get_rigid_body_angular_velocity( ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: rigid_body_angular_velocity()" ) ;
+
+  geomVector value(0.,0.,0.);
+
+  return (value);
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+std::tuple<double,double> DS_STL:: get_mass_and_density() const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_mass_and_density()" ) ;
+
+  // return ( m_geometric_rigid_body->get_mass_and_density() );
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+double DS_STL:: get_circumscribed_radius( ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_circumscribed_radius()" ) ;
+
+  return (m_geometric_rigid_body->get_circumscribed_radius());
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+geomVector const* DS_STL:: get_ptr_to_gravity_centre( ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_ptr_to_gravity_centre( )" ) ;
+
+  return (dynamic_cast<FS_RigidBody*>(m_geometric_rigid_body)
+                              ->get_ptr_to_gravity_centre());
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void DS_STL:: initialize_surface_variables( )
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: initialize_surface_variables" ) ;
+
+  if (m_surface_points.empty()) {
+     m_surface_points.reserve( Ntot );
+     m_surface_area.reserve( Ntot );
+     m_surface_normal.reserve( Ntot );
+     m_surface_Pforce.reserve( Ntot );
+     m_surface_Vforce.reserve( Ntot );
+     m_surface_Tgrad.reserve( Ntot );
+
+     geomVector vvv(3);
+
+     for (size_t i = 0; i < Ntot; ++i) {
+        m_surface_points.push_back( new geomVector(3) );
+        m_surface_area.push_back( new geomVector(1) );
+        m_surface_normal.push_back( new geomVector(3) );
+        m_surface_Pforce.push_back( vvv );
+        m_surface_Vforce.push_back( vvv );
+        m_surface_Tgrad.push_back( 0. );
+     }
+   }
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void DS_STL:: update_RB_position_and_velocity(geomVector const& pos,
+                                                    geomVector const& vel,
+                                                    geomVector const& ang_vel,
+                                   vector<geomVector> const& periodic_directions)
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: update_RB_position_and_velocity" ) ;
+
+  return (m_geometric_rigid_body->update_RB_position_and_velocity(pos,vel
+                                                                  ,ang_vel
+                                                         ,periodic_directions));
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+vector<geomVector*> DS_STL:: get_rigid_body_surface_points( ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_rigid_body_surface_points" ) ;
+
+  return (m_surface_points);
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+vector<geomVector*> DS_STL:: get_rigid_body_surface_normals( ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_rigid_body_surface_normals" ) ;
+
+  return (m_surface_normal);
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+vector<geomVector*> DS_STL:: get_rigid_body_surface_areas( ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_rigid_body_surface_areas" ) ;
+
+  return (m_surface_area);
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+vector<geomVector*> DS_STL:: get_rigid_body_haloZone( ) const
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: get_rigid_body_haloZone" ) ;
+
+  return (m_halo_zone);
+
+}
+
+
+
+//---------------------------------------------------------------------------
+void DS_STL:: update_Pforce_on_surface_point( size_t const& i
+                                                  , geomVector const& value )
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: update_Pforce_on_surface_point" ) ;
+
+  m_surface_Pforce[i] = value;
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void DS_STL:: update_Vforce_on_surface_point( size_t const& i
+                                                  , geomVector const& value )
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: update_Vforce_on_surface_point" ) ;
+
+  m_surface_Vforce[i] = value;
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void DS_STL:: update_Tgrad_on_surface_point( size_t const& i
+                                                 , double const& value )
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: update_Tgrad_on_surface_point" ) ;
+
+  m_surface_Tgrad[i] = value;
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void DS_STL:: correct_surface_discretization( FV_Mesh const* MESH )
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: correct_surface_discretization( )" ) ;
+
+  // vector<geomVector> const* p_pbc =
+  //                 dynamic_cast<FS_RigidBody*>(m_geometric_rigid_body)
+  //                             ->get_ptr_to_periodic_directions();
+  //
+  // if (p_pbc) {
+     boolVector const* periodic_comp = MESH->get_periodic_directions();
+     size_t dim = MESH->nb_space_dimensions() ;
+
+     for (size_t dir=0;dir < dim; dir++) {
+        bool is_periodic = periodic_comp->operator()( dir );
+
+        if (is_periodic) {
+           double isize = MESH->get_main_domain_max_coordinate(dir)
+                        - MESH->get_main_domain_min_coordinate(dir);
+           double imin = MESH->get_main_domain_min_coordinate(dir);
+
+           for (size_t i = 0; i < m_surface_area.size(); i++) {
+              m_surface_points[i]->operator()(dir) =
+                  m_surface_points[i]->operator()(dir)
+                - MAC::floor((m_surface_points[i]->operator()(dir)-imin)/isize)
+                  * isize;
+           }
+        }
+     }
+  // }
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void DS_STL:: write_surface_discretization( const std::string& file)
+//---------------------------------------------------------------------------
+{
+  MAC_LABEL( "DS_STL:: write_surface_discretization" ) ;
+
+  std::ofstream out;
+
+  out.open(file.c_str());
+  out << "x ,y ,z ,nx ,ny ,nz ,area ,Fpx ,Fpy ,Fpz ,Fvx ,Fvy ,Fvz, Tgrad" << endl;
+
+  for (size_t i = 0; i < m_surface_area.size(); i++) {
+     if ((m_surface_Pforce[i].calcNorm() != 0) ||
+         (m_surface_Vforce[i].calcNorm() != 0) ||
+         (m_surface_Tgrad[i] != 0))
+        out << m_surface_points[i]->operator()(0) << " ,"
+            << m_surface_points[i]->operator()(1) << " ,"
+            << m_surface_points[i]->operator()(2) << " ,"
+            << m_surface_normal[i]->operator()(0) << " ,"
+            << m_surface_normal[i]->operator()(1) << " ,"
+            << m_surface_normal[i]->operator()(2) << " ,"
+            << m_surface_area[i]->operator()(0) << " ,"
+            << m_surface_Pforce[i](0) << " ,"
+            << m_surface_Pforce[i](1) << " ,"
+            << m_surface_Pforce[i](2) << " ,"
+            << m_surface_Vforce[i](0) << " ,"
+            << m_surface_Vforce[i](1) << " ,"
+            << m_surface_Vforce[i](2) << " ,"
+            << m_surface_Tgrad[i] << endl;
+  }
+
+  out.close();
+
+}
