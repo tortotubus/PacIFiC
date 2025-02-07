732cc2d021bfc632c49c7ad44778f439a8525c75
houlette <houlette@student.ubc.ca>
2025-02-06 18:23:06 -0800

    add: rigid bodies vector construction

--- Cartesian/FLUID/Solvers/DLMFD/include/DLMFD_AllRigidBodies.hh
+++ Cartesian/FLUID/Solvers/DLMFD/include/DLMFD_AllRigidBodies.hh
@@ -4,7 +4,9 @@
 #include <size_t_vector.hh>
 #include <size_t_array2D.hh>
 #include <FS_AllRigidBodies.hh>
-using std::istream;
+#include <DLMFD_RigidBody.hh>
+#include <FV_DiscreteField.hh>
+using namespace std;
 
 /** @brief The class DS_AllRigidBodies.
 
@@ -15,7 +17,11 @@ The array of all rigid bodies in the Fictitious Domain solver.
 class DLMFD_AllRigidBodies
 {
 public: //-----------------------------------------------------------------
-    DLMFD_AllRigidBodies(size_t &dim, istringstream &in, bool const &are_particles_fixed);
+    DLMFD_AllRigidBodies(size_t &dim,
+                         istringstream &in,
+                         bool const &are_particles_fixed,
+                         FV_DiscreteField const *UF,
+                         FV_DiscreteField const *PF);
 
     ~DLMFD_AllRigidBodies();
 
@@ -26,6 +32,7 @@ public: //-----------------------------------------------------------------
 protected: //--------------------------------------------------------------
 private:   //----------------------------------------------------------------
     FS_AllRigidBodies *ptr_FSallrigidbodies;
+    vector<DLMFD_RigidBody *> vec_ptr_DLMFDallrigidbodies;
     size_t RBs_number;
 };
 
--- Cartesian/FLUID/Solvers/DLMFD/src/DLMFD_AllRigidBodies.cc
+++ Cartesian/FLUID/Solvers/DLMFD/src/DLMFD_AllRigidBodies.cc
@@ -1,13 +1,27 @@
 #include <DLMFD_AllRigidBodies.hh>
-#include <FS_AllRigidBodies.hh>
+#include <DLMFD_RigidBody_BuilderFactory.hh>
+using namespace std;
 
 //---------------------------------------------------------------------------
-DLMFD_AllRigidBodies::DLMFD_AllRigidBodies(size_t &dim, istringstream &in, bool const &are_particles_fixed)
+DLMFD_AllRigidBodies::DLMFD_AllRigidBodies(size_t &dim,
+                                           istringstream &in,
+                                           bool const &are_particles_fixed,
+                                           FV_DiscreteField const *UF,
+                                           FV_DiscreteField const *PF)
 //---------------------------------------------------------------------------
 {
    MAC_LABEL("DLMFD_AllRigidBodies:: DLMFD_AllRigidBodies");
    ptr_FSallrigidbodies = new FS_AllRigidBodies(dim, in, are_particles_fixed);
    RBs_number = ptr_FSallrigidbodies->get_number_rigid_bodies();
+
+   DLMFD_RigidBody *ptr_dlmfd_rb = NULL;
+
+   for (size_t i = 0; i < RBs_number; ++i)
+   {
+      vec_ptr_DLMFDallrigidbodies.push_back(ptr_dlmfd_rb);
+      vec_ptr_DLMFDallrigidbodies[i] = DLMFD_RigidBody_BuilderFactory::create(
+          ptr_FSallrigidbodies->get_ptr_rigid_body(i));
+   }
 }
 
 //---------------------------------------------------------------------------
@@ -30,5 +44,6 @@ size_t DLMFD_AllRigidBodies::get_number_rigid_bodies() const
 void DLMFD_AllRigidBodies::update(istringstream &in)
 //---------------------------------------------------------------------------
 {
-   printf("Hello World from Rigid body updating \n");
+   cout << "Hello World from Rigid body updating"
+        << endl;
 }
