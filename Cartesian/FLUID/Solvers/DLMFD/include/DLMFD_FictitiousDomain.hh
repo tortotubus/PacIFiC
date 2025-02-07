7d208eb25bdc17fc3d59df9c0315535649215f3c
houlette <houlette@student.ubc.ca>
2025-02-06 18:24:29 -0800

    add: RBs updating and DLMFD in DLMFD_FictitiousDomain

--- Cartesian/FLUID/Solvers/DLMFD/include/DLMFD_FictitiousDomain.hh
+++ Cartesian/FLUID/Solvers/DLMFD/include/DLMFD_FictitiousDomain.hh
@@ -2,6 +2,10 @@
 #define DLMFD_FictitiousDomain_HH
 
 #include <MAC_DoubleVector.hh>
+#include <FS_SolidPlugIn.hh>
+#include <DLMFD_AllRigidBodies.hh>
+#include <FV_DomainAndFields.hh>
+#include <MAC_ModuleExplorer.hh>
 
 /** @brief The Class DLMFD_FictitiousDomain.
 
@@ -13,17 +17,45 @@ Multiplier/Fictitious Domain method.
 class DLMFD_FictitiousDomain : public MAC_Object
 {
 public: //-----------------------------------------------------------------
-   static DLMFD_FictitiousDomain *create(MAC_Object *a_owner);
+   static DLMFD_FictitiousDomain *create(MAC_Object *a_owner, FV_DomainAndFields const *dom,
+                                         MAC_ModuleExplorer const *exp);
 
    void do_one_inner_iteration();
+
+   void update_rigid_bodies();
 
    void run_DLMFD_UzawaSolver();
 
 protected: //--------------------------------------------------------------
 private:   //----------------------------------------------------------------
-   DLMFD_FictitiousDomain(MAC_Object *a_owner);
+   DLMFD_FictitiousDomain(MAC_Object *a_owner, FV_DomainAndFields const *dom,
+                          MAC_ModuleExplorer const *exp);
 
    ~DLMFD_FictitiousDomain();
+
+   FV_DiscreteField *UU;
+   FV_DiscreteField *PP;
+
+   // Physical Parameters
+   size_t dim;
+   double density;
+   double viscosity;
+
+   // Numerical parameters
+   size_t sub_prob_number;
+
+   // Grains3D variables
+   string solidSolverType;
+   FS_SolidPlugIn *solidSolver;
+   bool b_solidSolver_parallel;
+   string solidSolver_insertionFile;
+   string solidSolver_simulationFile;
+   istringstream *solidFluid_transferStream;
+   DLMFD_AllRigidBodies *allrigidbodies;
+   bool are_particles_fixed;
+
+   // Restart
+   bool b_restart;
 };
 
 #endif
--- Cartesian/FLUID/Solvers/DLMFD/src/DLMFD_FictitiousDomain.cc
+++ Cartesian/FLUID/Solvers/DLMFD/src/DLMFD_FictitiousDomain.cc
@@ -1,10 +1,34 @@
 #include <DLMFD_FictitiousDomain.hh>
+#include <FS_SolidPlugIn_BuilderFactory.hh>
+#include <FV_Mesh.hh>
+using namespace std;
 
 //---------------------------------------------------------------------------
-DLMFD_FictitiousDomain::DLMFD_FictitiousDomain(MAC_Object *a_owner) : MAC_Object(a_owner)
+DLMFD_FictitiousDomain::DLMFD_FictitiousDomain(MAC_Object *a_owner,
+                                               FV_DomainAndFields const *dom,
+                                               MAC_ModuleExplorer const *exp) : MAC_Object(a_owner)
 //--------------------------------------------------------------------------
 {
    MAC_LABEL("DLMFD_FictitiousDomain:: DLMFD_FictitiousDomain");
+
+   // Create solid readable files
+   solidSolverType = "Grains3D";
+   b_solidSolver_parallel = false;
+   solidSolver_insertionFile = "Grains/Init/insert.xml";
+   solidSolver_simulationFile = "Grains/Res/simul.xml";
+   int error = 0;
+
+   // Create rigid bodies object
+   solidSolver = FS_SolidPlugIn_BuilderFactory::create(solidSolverType,
+                                                       solidSolver_insertionFile, solidSolver_simulationFile, density, false,
+                                                       b_restart, dom->primary_grid()->get_smallest_constant_grid_size(),
+                                                       b_solidSolver_parallel, error);
+
+   solidFluid_transferStream = NULL;
+   solidSolver->getSolidBodyFeatures(solidFluid_transferStream);
+
+   allrigidbodies = new DLMFD_AllRigidBodies(dim, *solidFluid_transferStream,
+                                             are_particles_fixed, UU, PP);
 }
 
 //---------------------------------------------------------------------------
@@ -15,11 +39,13 @@ DLMFD_FictitiousDomain::~DLMFD_FictitiousDomain(void)
 }
 
 //---------------------------------------------------------------------------
-DLMFD_FictitiousDomain *DLMFD_FictitiousDomain::create(MAC_Object *a_owner)
+DLMFD_FictitiousDomain *DLMFD_FictitiousDomain::create(MAC_Object *a_owner,
+                                                       FV_DomainAndFields const *dom,
+                                                       MAC_ModuleExplorer const *exp)
 //---------------------------------------------------------------------------
 {
    MAC_LABEL("DLMFD_FictitiousDomain:: create");
-   DLMFD_FictitiousDomain *result = new DLMFD_FictitiousDomain(a_owner);
+   DLMFD_FictitiousDomain *result = new DLMFD_FictitiousDomain(a_owner, dom, exp);
    return (result);
 }
 
@@ -27,12 +53,32 @@ DLMFD_FictitiousDomain *DLMFD_FictitiousDomain::create(MAC_Object *a_owner)
 void DLMFD_FictitiousDomain::do_one_inner_iteration()
 //---------------------------------------------------------------------------
 {
+
+   update_rigid_bodies();
    run_DLMFD_UzawaSolver();
+}
+
+//---------------------------------------------------------------------------
+void DLMFD_FictitiousDomain::update_rigid_bodies()
+//---------------------------------------------------------------------------
+{
+   sub_prob_number = 3;
+   MAC::out() << "-----------------------------------------" << "-------------" << endl;
+   MAC::out() << "Sub-problem " << sub_prob_number
+              << " : Rigid Bodies updating" << endl;
+   MAC::out() << "-----------------------------------------" << "-------------" << endl;
+   allrigidbodies->update(*solidFluid_transferStream);
 }
 
 //---------------------------------------------------------------------------
 void DLMFD_FictitiousDomain::run_DLMFD_UzawaSolver()
 //---------------------------------------------------------------------------
 {
-   printf("Hello World from Uzawa algorithm \n");
+   sub_prob_number = 4;
+   MAC::out() << "-----------------------------------------" << "-------------" << endl;
+   MAC::out() << "Sub-problem " << sub_prob_number
+              << " : DLMFD processing" << endl;
+   MAC::out() << "-----------------------------------------" << "-------------" << endl;
+   cout << "Hello World from Uzawa algorithm"
+        << endl;
 }
