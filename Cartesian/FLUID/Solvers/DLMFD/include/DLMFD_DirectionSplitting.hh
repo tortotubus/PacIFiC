fdd6b81807441133d1e981f6ca002f5c25b7fc9a
houlette <houlette@student.ubc.ca>
2025-04-22 13:25:05 -0700

    add: Direction Splitting formulation

---
+++ Cartesian/FLUID/Solvers/DLMFD/include/DLMFD_DirectionSplitting.hh
@@ -0,0 +1,354 @@
+#ifndef DLMFD_DirectionSplitting_HH
+#define DLMFD_DirectionSplitting_HH
+
+#include <mpi.h>
+#include <FV_OneStepIteration.hh>
+#include <geomVector.hh>
+#include <PAC_computingtime.hh>
+#include <boolVector.hh>
+#include <PAC_solvercomputingtime.hh>
+#include <MAC_DoubleVector.hh>
+#include <DS_PID.hh>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <utility>
+#include <DLMFD_FictitiousDomain.hh>
+using namespace std;
+
+class MAC_Communicator;
+class FV_DiscreteField;
+class LA_Vector;
+class LA_SeqVector;
+class DLMFD_DirectionSplittingSystem;
+class LA_SeqMatrix;
+
+/** @brief The Class DLMFD_DirectionSplitting.
+
+Server for the resolution of the unsteady momentum equation by a second order
+time integrator and a Finite Volume MAC scheme on rectangular grids.
+
+@author A. Goyal - Pacific project 2025 */
+
+/** @brief MPIVar include all vectors required while message passing */
+struct MPIVarNS
+{
+    size_t *size;
+    double ***send;
+    double ***receive;
+};
+
+class DLMFD_DirectionSplitting : public FV_OneStepIteration, public PAC_ComputingTime, public PAC_SolverComputingTime
+{
+public: //-----------------------------------------------------------------
+        //-- Substeps of the step by step progression
+    /** @name Substeps of the step by step progression */
+    //@{
+    /** @brief Tasks performed at initialization of the algorithm, before
+    starting the time stepping loop
+    @param t_it time iterator */
+    virtual void do_before_time_stepping(FV_TimeIterator const *t_it,
+                                         std::string const &basename);
+
+    /** @brief Perform one time step
+    @param t_it time iterator */
+    virtual void do_one_inner_iteration(FV_TimeIterator const *t_it);
+
+    /** @brief Tasks performed at initialization of each time step
+    @param t_it time iterator */
+    virtual void do_before_inner_iterations_stage(
+        FV_TimeIterator const *t_it);
+
+    /** @brief Tasks performed after of each time step
+    @param t_it time iterator */
+    virtual void do_after_inner_iterations_stage(
+        FV_TimeIterator const *t_it);
+
+    /** @brief Tasks performed at the end of the time stepping loop */
+    virtual void do_after_time_stepping(void);
+
+    /** @brief Save additional data than fields
+    @param t_it time iterator
+    @param cycleNumber cycle number */
+    virtual void do_additional_savings(FV_TimeIterator const *t_it,
+                                       int const &cycleNumber);
+    //@}
+
+    //-- Projection-Translation methods
+
+    /** @name Projection-Translation methods */
+    //@{
+    /** @brief Projection of the field on the translated position of the grid
+     */
+    void fields_projection();
+    //@}
+
+protected: //--------------------------------------------------------------
+private:   //----------------------------------------------------------------
+           //-- Constructors & Destructor
+    /** @name Constructors & Destructor */
+    //@{
+    /** @brief Destructor */
+    virtual ~DLMFD_DirectionSplitting(void);
+
+    /** @brief Copy constructor */
+    DLMFD_DirectionSplitting(DLMFD_DirectionSplitting const &other);
+
+    /** @brief Constructor with arguments
+    @param a_owner the MAC-based object
+    @param exp to read the data file */
+    DLMFD_DirectionSplitting(MAC_Object *a_owner,
+                             FV_DomainAndFields const *dom,
+                             MAC_ModuleExplorer const *exp);
+
+    /** @brief Constructor without argument */
+    DLMFD_DirectionSplitting(void);
+
+    /** @brief Create a clone
+    @param a_owner the MAC-based object
+    @param dom mesh and fields
+    @param prms set of parameters
+    @param exp to read the data file */
+    virtual DLMFD_DirectionSplitting *create_replica(MAC_Object *a_owner,
+                                                     FV_DomainAndFields const *dom,
+                                                     MAC_ModuleExplorer *exp) const;
+
+    //@}
+
+    //-- Basic discrete system building
+
+    /** @name Basic discrete system building */
+    //@{
+    /** @brief Call the function to assemble 1D matrices
+    for both velocity and pressure field*/
+    void assemble_1D_matrices(FV_DiscreteField const *FF, FV_TimeIterator const *t_it);
+
+    /** @brief Assemble 1D matrices for both velocity
+    and pressure field in all directions */
+    void assemble_field_matrix(FV_DiscreteField const *FF, FV_TimeIterator const *t_it);
+
+    /** @brief Assemble 1D schur matrices for both
+    velocity and pressure field in all directions */
+    void assemble_field_schur_matrix(FV_DiscreteField const *FF);
+
+    /** @brief Assemble advection term for Upwind spacial scheme */
+    double assemble_advection_Upwind(size_t const &advecting_level, double const &coef, size_t const &advected_level, size_t const &i, size_t const &j, size_t const &k, size_t const &component);
+
+    /** @brief Assemble advection term for Centered spacial scheme */
+    double assemble_advection_Centered(size_t const &advecting_level, double const &coef, size_t const &advected_level, size_t const &i, size_t const &j, size_t const &k, size_t const &component);
+
+    /** @brief Assemble advection term for Centered spacial scheme with FD corrections*/
+    double assemble_advection_Centered_FD(double const &coef, size_t const &i, size_t const &j, size_t const &k, size_t const &comp, size_t const &level);
+
+    // /** @brief Assemble advection term for Centered spacial scheme with CutCell corrections */
+    // double assemble_advection_Centered_CutCell(FV_TimeIterator const *t_it, double const &coef, size_t const &i, size_t const &j, size_t const &k, size_t const &comp, size_t const &level);
+
+    /** @brief Assemble advection term for TVD spacial scheme */
+    double assemble_advection_TVD(size_t const &advecting_level, double const &coef, size_t const &advected_level, size_t const &i, size_t const &j, size_t const &k, size_t const &component) const;
+
+    /** @brief Assemble rhs for velocity in any direction */
+    double velocity_local_rhs(size_t const &j, size_t const &k, double const &gamma, FV_TimeIterator const *t_it, size_t const &comp, size_t const &dir);
+
+    /** @brief Compute first derivative */
+    std::tuple<double, double>
+    compute_first_derivative(size_t const &comp, size_t const &i, size_t const &j, size_t const &k, size_t const &dir, size_t const &level);
+
+    /** @brief Compute diffusive term of velocity field from previous timestep */
+    double compute_un_component(size_t const &comp, size_t const &i, size_t const &j, size_t const &k, size_t const &dir, size_t const &level);
+    /** @brief Compute diffusive term of velocity field from previous timestep */
+    double compute_un_component_FD(size_t const &comp, size_t const &i, size_t const &j, size_t const &k, size_t const &dir, size_t const &level);
+    // /** @brief Compute diffusive term of velocity field from previous timestep */
+    // double compute_un_component_FV(size_t const &comp, size_t const &i, size_t const &j, size_t const &k, size_t const &dir, size_t const &level);
+    /** @brief Compute diffusive term of pressure field from previous timestep */
+    double compute_p_component(size_t const &comp, size_t const &i, size_t const &j, size_t const &k);
+    /** @brief Compute advective term based on either Upwind or TVD spacial scheme */
+    double compute_adv_component(FV_TimeIterator const *t_it, size_t const &comp, size_t const &i, size_t const &j, size_t const &k);
+    /** @brief Assemble rhs term after calling compute_**_component */
+    void assemble_DS_un_at_rhs(FV_TimeIterator const *t_it, double const &gamma);
+
+    void assemble_velocity_diffusion_terms();
+
+    void assemble_velocity_advection_terms(FV_TimeIterator const *t_it);
+
+    void calculate_row_indexes(FV_DiscreteField const *FF);
+
+    void compute_velocity_divergence(FV_DiscreteField const *FF);
+
+    double divergence_of_U_noCorrection(size_t const &i, size_t const &j, size_t const &k, size_t const &comp, size_t const &level);
+
+    /** @brief Call functions to assemble rhs for pressure or velocity fields in any direction */
+    double assemble_local_rhs(size_t const &j, size_t const &k, double const &gamma, FV_TimeIterator const *t_it, size_t const &comp, size_t const &dir, size_t const &field);
+
+    /** @brief Assemble divergence for penelty step */
+    double calculate_velocity_divergence_FD(size_t const &i, size_t const &j, size_t const &k, size_t const &level);
+
+    // /** @brief Assemble divergence for penelty step */
+    // double calculate_velocity_divergence_cutCell(size_t const &i, size_t const &j, size_t const &k, size_t const &level);
+
+    double assemble_velocity_gradients(class doubleVector &grad, size_t const &i, size_t const &j, size_t const &k, size_t const &level);
+
+    // void detect_fresh_cells_and_neighbours();
+
+    void calculate_divergence_weighting(FV_TimeIterator const *t_it);
+
+    double pressure_local_rhs(size_t const &j, size_t const &k, FV_TimeIterator const *t_it, size_t const &dir);
+
+    // /** @brief Initialize the velocity on the velocity nodes in MAC grid*/
+    // void initialize_grid_nodes_on_rigidbody(vector<size_t> const &list);
+
+    void ugradu_initialization();
+
+    // void correct_pressure_1st_layer_solid(size_t const &level);
+
+    // void correct_pressure_2nd_layer_solid(size_t const &level);
+
+    void correct_mean_pressure(size_t const &level);
+
+    void predicted_pressure_drop(FV_TimeIterator const *t_it);
+
+    double get_current_mean_flow_speed();
+
+    /** @brief Solve interface unknowns for
+    both fields in any particular direction */
+    void solve_interface_unknowns(FV_DiscreteField *FF, double const &gamma, FV_TimeIterator const *t_it, size_t const &comp, size_t const &dir, size_t const &level);
+    /** @brief Unpack the interface variable sent
+    by master processor to slave processor */
+    void unpack_ue(size_t const &comp, double *received_data, size_t const &dir, size_t const &p, size_t const &field);
+    /** @brief Unpack the data sent by "data_packing" and compute
+    the interface unknown; and pack ue for sending to slave processor */
+    void unpack_compute_ue_pack(size_t const &comp, size_t const &dir, size_t const &p, size_t const &field);
+
+    /** @brief Pack Aei*(Aii)-1*fi and fe for sending to master processor */
+    void data_packing(FV_DiscreteField const *FF, size_t const &p, double const &fe, size_t const &comp, size_t const &dir);
+    /** @brief Compute Aei*(Aii)-1*fi required to compute interface unknown */
+    void compute_Aei_ui(struct TDMatrix *arr, struct LocalVector *VEC, size_t const &comp, size_t const &dir, size_t const &r_index);
+
+    /** @brief Solve interface unknown for all cases */
+    void DS_interface_unknown_solver(LA_SeqVector *rhs, size_t const &comp, size_t const &dir, size_t const &field, size_t const &r_index);
+
+    /** @brief Solve i in j and k; e.g. solve x in y ank z */
+    void Solve_i_in_jk(FV_DiscreteField *FF, FV_TimeIterator const *t_it, size_t const &dir_i, size_t const &dir_j, size_t const &dir_k, double const &gamma, size_t const &level);
+
+    /** Pressure predictor */
+    void NS_first_step(FV_TimeIterator const *t_it);
+
+    /** Velocity update */
+    void NS_velocity_update(FV_TimeIterator const *t_it);
+
+    /** Pressure update (penalty step) */
+    void NS_pressure_update(FV_TimeIterator const *t_it);
+
+    /** Pressure correction */
+    void NS_final_step(FV_TimeIterator const *t_it);
+
+    void write_output_field(FV_DiscreteField const *FF);
+
+    void output_L2norm_divergence();
+
+    void output_L2norm_pressure(size_t const &level);
+
+    void output_L2norm_velocity(size_t const &level);
+
+    doubleVector compute_outOfDomain_Pressure(size_t const &level);
+
+    /** @brief Compute velocity change from one time step to the
+    next one with the direction splitting solution method */
+    double compute_DS_velocity_change(void);
+    //@}
+
+    //-- Direction splitting communicators
+
+    /** @name Direction splitting communicators */
+    /** @brief Create the sub-communicators */
+    void create_DS_subcommunicators(void);
+    void processor_splitting(int const &color, int const &key, size_t const &dir);
+
+    void allocate_mpi_variables(FV_DiscreteField const *FF);
+    void deallocate_mpi_variables();
+
+    /** @brief Free the sub-communicators */
+    void free_DS_subcommunicators(void);
+
+    //@}
+
+    //-- Projection-Translation methods
+
+    /** @name Projection-Translation methods */
+    //@{
+    /** @brief Build the field projection-translation interpolations */
+    void build_links_translation();
+    //@}
+
+private: //----------------------------------------------------------------
+         //-- Class attributes
+    //-- Attributes
+
+    FV_DiscreteField *UF;
+    FV_DiscreteField *PF;
+
+    DLMFD_DirectionSplittingSystem *GLOBAL_EQ;
+    DS_PID *controller;
+
+    size_t nb_procs;
+    size_t my_rank;
+    size_t is_master;
+    size_t dim;
+    // 0th element for P and 1st element for U
+    size_t nb_comps[2];
+
+    MAC_Communicator const *macCOMM;
+    MPI_Comm DS_Comm_i[3];
+
+    int rank_in_i[3];
+    int nb_ranks_comm_i[3];
+
+    // [0,1] are for pressure and velocity;[0,1,2] are for x, y and z directions
+    struct MPIVarNS first_pass[2][3];
+    // [0,1] are for pressure and velocity;[0,1,2] are for x, y and z directions
+    struct MPIVarNS second_pass[2][3];
+    // [0,1] are for pressure and velocity;[0,1,2] are for x, y and z directions
+    struct MPIVarNS data_for_S[2][3];
+
+    double mu;
+    double kai;
+    string AdvectionScheme;
+    string StencilCorrection;
+    bool is_CConlyDivergence;
+    double FluxRedistThres;
+    size_t AdvectionTimeAccuracy;
+    double rho;
+    bool b_restart;
+    bool is_solids;
+
+    bool is_stressCal;
+    string ViscousStressOrder;
+    string PressureStressOrder;
+    double surface_cell_scale;
+    size_t stressCalFreq;
+    bool is_par_motion;
+
+    // Grid motion
+    bool b_projection_translation;
+    int outOfDomain_boundaryID;
+
+    double Qold;
+
+    boolVector const *P_periodic_comp;
+    boolVector const *U_periodic_comp;
+    MAC_DoubleVector *gravity_vector;
+    geomVector split_gravity_vector;
+    geomVector gravity_vector_geom;
+    bool is_periodic[2][3];
+
+    bool exceed, turn;
+
+    // DLMFD solver
+    DLMFD_FictitiousDomain *dlmfd_solver;
+
+    // Post-processing
+    string resultsDirectory;
+};
+
+#endif
---
+++ Cartesian/FLUID/Solvers/DLMFD/include/DLMFD_DirectionSplittingSystem.hh
@@ -0,0 +1,272 @@
+#ifndef REG_HEAT_EQUATION_SYSTEM_HH
+#define REG_HEAT_EQUATION_SYSTEM_HH
+
+#include <MAC_Object.hh>
+#include <utility>
+#include <boolVector.hh>
+#include <size_t_array2D.hh>
+#include <doubleArray2D.hh>
+#include <vector>
+using namespace std;
+
+class MAC_ModuleExplorer;
+class MAC_Communicator;
+class MAC_Timer;
+class size_t_vector;
+class intVector;
+class doubleVector;
+class doubleArray2D;
+class LA_Matrix;
+class LA_SeqMatrix;
+class LA_Vector;
+class LA_SeqVector;
+class LA_Scatter;
+class LA_Solver;
+class LA_CRSmatrix;
+class FV_SystemNumbering;
+class FV_DiscreteField;
+class FV_TimeIterator;
+
+/** For set of variables to pass from NavierStokes to System */
+struct NS2System
+{
+    bool is_solids_;
+    bool is_stressCal_;
+};
+
+/** @brief TDMatrix include all elements of block matrices (ii,ie,ei,ee) */
+struct TDMatrix
+{
+    LA_SeqVector ***ii_main;
+    LA_SeqVector ***ii_super;
+    LA_SeqVector ***ii_sub;
+    LA_SeqMatrix ***ie;
+    LA_SeqMatrix ***ei;
+    LA_SeqMatrix ***ee;
+};
+
+/** @brief Product matrix is composed of products
+elements of block matrices (ii,ie,ei,ee) */
+struct ProdMatrix
+{
+    LA_SeqMatrix **ei_ii_ie;
+    LA_SeqVector **ii_ie;
+    LA_SeqVector **result;
+};
+
+/** @brief LocalVector to be used in storing the
+local values and interface values of DOF */
+struct LocalVector
+{
+    LA_SeqVector **local_T;
+    LA_SeqVector **local_solution_T;
+    LA_SeqVector **T;
+    LA_SeqVector **interface_T;
+};
+
+/** @brief NodeProp to be used to store the
+nodes properties due to presence of solid particles in the domian */
+struct NodeProp
+{
+    LA_SeqVector *void_frac;
+    // void_fraction of the node due to particle
+    LA_SeqVector *parID;
+    // ID of solid particle on the node
+    LA_SeqVector *bound_cell;
+    // Stores the boundary cell presence in the solids; 1 == 1st boundary cell
+};
+
+/** @brief The Class DLMFD_DirectionSplittingSystem.
+
+Matrix systems for the resolution of the heat equation.
+
+@author A. Goyal - Pacific project 2022 */
+
+class DLMFD_DirectionSplittingSystem : public MAC_Object
+{
+private: //----------------------------------------------------------
+         //-- Constructors & Destructor
+    /** @name Constructors & Destructor */
+    //@{
+    /** @brief Constructor without argument */
+    DLMFD_DirectionSplittingSystem(void);
+
+    /** @brief Destructor */
+    ~DLMFD_DirectionSplittingSystem(void);
+
+    /** @brief Copy constructor */
+    DLMFD_DirectionSplittingSystem(DLMFD_DirectionSplittingSystem const &other);
+
+    /** @brief Constructor with arguments
+    @param a_owner the MAC-based object
+    @param exp to read the data file
+    @param mac_UF FV velocity field */
+    DLMFD_DirectionSplittingSystem(MAC_Object *a_owner,
+                                   MAC_ModuleExplorer const *exp,
+                                   FV_DiscreteField *mac_UF,
+                                   FV_DiscreteField *mac_PF,
+                                   struct NS2System const &fromNS);
+    //@}
+
+public: //-----------------------------------------------------------
+        //-- Instance delivery and initialization
+    /** @name Instance delivery and initialization */
+    //@{
+    /** @brief Create and initialize an instance of DLMFD_DirectionSplittingSystem
+    @param a_owner the MAC-based object
+    @param exp to read the data file
+    @param mac_UF FV velocity field */
+    static DLMFD_DirectionSplittingSystem *create(MAC_Object *a_owner,
+                                                  MAC_ModuleExplorer const *exp,
+                                                  FV_DiscreteField *mac_UF,
+                                                  FV_DiscreteField *mac_PF,
+                                                  struct NS2System const &transfer);
+    //@}
+
+    //-- Access
+
+    /** @name Access */
+    //@{
+    /** @brief Return the matrix system of spacial discretization */
+    TDMatrix *get_A(size_t const &field);
+    /** @brief Return the Schur complement of spacial discretization */
+    TDMatrix *get_Schur(size_t const &field);
+    /** @brief Return the (presence/absence) of particle vector */
+    NodeProp get_node_property(size_t const &field, size_t const &time_level);
+    /** @brief Return the divergence on pressure node */
+    doubleArray2D *get_node_divergence(size_t const &field);
+    /** @brief Return the velocity diffusive terms */
+    vector<doubleVector *> get_velocity_diffusion();
+    /** @brief Return the velocity advection terms */
+    vector<doubleVector *> get_velocity_advection();
+    /** @brief Return the velocity field face fractions */
+    vector<doubleArray2D *> get_velocity_face_fractions();
+    vector<doubleArray2D *> get_velocity_normalRB();
+    /** @brief Return the local vector with a vector of row index */
+    size_t_array2D *get_row_indexes(size_t const &field, size_t const &dir, size_t const &comp);
+
+    /** @brief Return the Schur complement of
+    Schur complement in case of periodic domain */
+    TDMatrix *get_DoubleSchur(size_t const &field);
+    /** @brief Return the product matrix of Schur complement */
+    ProdMatrix *get_SchurP(size_t const &field);
+    /** @brief Return RHS for the Schur complement */
+    LocalVector *get_Schur_VEC(size_t const &field);
+    /** @brief Return the product matrix of spacial discretization */
+    ProdMatrix *get_Ap(size_t const &field);
+    /** @brief Return RHS for the matrix system of spacial discretization */
+    LocalVector *get_VEC(size_t const &field);
+
+    //-- Solver
+
+    /** @name Solvers */
+    //@{
+    /** @brief Solve the DS splitting problem in x by performing the
+    matrix-vector product A_x^-1.Vx and transfer in the distributed vector */
+    void DLMFD_DirectionSplitting_solver(FV_DiscreteField *FF, size_t const &j, size_t const &k, size_t const &min_i, size_t const &comp, size_t const &dir, size_t const &r_index, size_t const &level);
+
+    //@}
+
+    //-- Output methods
+
+    /** @name Output methods */
+    //@{
+    /** @brief Display matrices and vectors for debugging purposes */
+    void display_debug(void);
+    //@}
+
+    /** @brief Calls interior function for different conditions
+    to compute the product of Aei*inv(Aii)*Aie */
+    void compute_product_matrix(struct TDMatrix *arr, struct ProdMatrix *prr, size_t const &comp, size_t const &dir, size_t const &field, size_t const &r_index);
+    /** @brief Compute the product of Aei*inv(Aii)*Aie in any
+    direction for any field*/
+    void compute_product_matrix_interior(struct TDMatrix *arr, struct ProdMatrix *prr, size_t const &comp, size_t const &column, size_t const &dir, size_t const &r_index);
+
+    //-- Utilities
+
+    /** @name Utilities */
+    //@{
+    /** @brief Solve Linear system mat_A*x = rhs with only three
+    vectors of mat_A(x,y,z) using thomas algorithm  */
+    void mod_thomas_algorithm(TDMatrix *arr, LA_SeqVector *rhs, size_t const &comp, size_t const &dir, size_t const &r_index);
+    /** @brief Compute the modified super diagonal for thomas algorithm  */
+    void pre_thomas_treatment(size_t const &comp, size_t const &dir, struct TDMatrix *arr, size_t const &r_index);
+    //@}
+
+protected: //--------------------------------------------------------
+private:   //----------------------------------------------------------
+    /** @name Initialize matrices & vectors */
+    //@{
+    /** @brief Create matrices & vectors (without allocating memory)
+    @param exp to read the data file */
+    void build_system(MAC_ModuleExplorer const *exp);
+
+    /** @brief Allocate memory for matrices & vectors */
+    void re_initialize(void);
+    //@}
+
+    //-- Attributes
+    FV_DiscreteField *UF;
+    FV_DiscreteField *PF;
+
+    // Local vector to store row indexes for each
+    // field (i.e. 2)
+    // and direction (i.e. 3)
+    size_t_array2D **row_index[2][3];
+
+    // Unknowns numbering
+    FV_SystemNumbering *UF_NUM;
+    FV_SystemNumbering *PF_NUM;
+
+    // Direction splitting matrices
+    LA_SeqMatrix *MAT_velocityUnsteadyPlusDiffusion_1D;
+
+    // Spacitial discretization matrices
+    // [0,1] are for pressure and velocity;[0,1,2] are for x, y and z directions
+    struct TDMatrix A[2][3];
+    // [0,1] are for pressure and velocity;[0,1,2] are for x, y and z directions
+    struct ProdMatrix Ap[2][3];
+    // [0,1] are for pressure and velocity;[0,1,2] are for x, y and z directions
+    struct LocalVector VEC[2][3];
+
+    // Schur complement matrices
+    // [0,1] are for pressure and velocity;[0,1,2] are for x, y and z directions
+    struct TDMatrix Schur[2][3];
+    struct ProdMatrix SchurP[2][3];
+    struct LocalVector Schur_VEC[2][3];
+
+    // Schur complement of Schur complement
+    struct TDMatrix DoubleSchur[2][3];
+
+    // Particle structures
+    // 2 rows are for fields; 2 columns are for time level (current and last)
+    struct NodeProp node[2][2];
+    // 0 current timestep, 1 last time step
+    vector<doubleArray2D *> vel_divergence;
+    // Local vector to store diffusive terms
+    vector<doubleVector *> vel_diffusion;
+    // Local vector to store advection terms
+    vector<doubleVector *> vel_advection;
+
+    size_t dim;
+    MAC_Communicator const *pelCOMM;
+    size_t nb_comps[2];
+
+    /** Processor positions in x,y,z */
+    size_t proc_pos_in_i[3];
+    /** Number of Processors in x,y,z */
+    size_t nb_procs_in_i[3];
+
+    bool is_solids;
+    bool is_stressCal;
+    size_t Npart;
+    string level_set_type;
+    double Nmax;
+    double Rpart, ar;
+
+    bool is_periodic[2][3];
+    boolVector const *U_periodic_comp;
+    boolVector const *P_periodic_comp;
+};
+
+#endif
