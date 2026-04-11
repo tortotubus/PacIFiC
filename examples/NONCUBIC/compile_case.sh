e571fa9bfd5916d95996559a20d011156fa362e5
Conor E. Olive <conor@conorolive.com>
2026-04-10 17:16:49 -0700

    move + add -ltinyrenderer

--- examples/NONCUBIC/compile_case.sh
+++ examples/NONCUBIC/compile_case.sh
@@ -1,5 +1,3 @@
-# source ~/pacific-caps/Env/PacIFiC-OpenMPI-4.1.2-GNU-11.4.0_Ubuntu22.env.sh
-
 PACIFIC_THIRDPARTY=${PACIFIC_BUILDDIR_ABS}/third_party
 PACIFIC_OCTREE=${PACIFIC_SRCDIR_ABS}/Octree
 
@@ -9,10 +7,8 @@ CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 -DINITIAL_FLOW_FIELD=0 \
         $1 -o "${1%.*}" \
 	-I${PACIFIC_OCTREE} \
 	-L${PACIFIC_THIRDPARTY}/lib64 \
-	-lglutils -lfb_tiny -lwsserver -lm \
+	-lglutils -lfb_tiny -lwsserver -lm -ltinyrenderer \
         -grid=multigrid3D 
-
-
 
 chmod +x "${1%.*}"
 mkdir -p Res
